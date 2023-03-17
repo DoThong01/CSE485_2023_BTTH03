0221c13b67ed1a0699a31832dd935bd1b780af0b
Đỗ Thông <DoThong01>
2023-03-17 13:20:04 +0700

    .

--- Utilities/my_email_server.php
+++ Utilities/my_email_server.php
@@ -14,13 +14,13 @@ include 'email_server_interface.php';
                 $mail->isSMTP();                                            //Send using SMTP
                 $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
                 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                $mail->Username   = 'damvietvuong123@gmail.com';                     //SMTP username
-                $mail->Password   = 'zcdpgnaabjcymphs';                               //SMTP password
+                $mail->Username   = 'dothong27112001@gmail.com';                     //SMTP username
+                $mail->Password   = 'zrdrhwbozmteqnwp';                               //SMTP password
                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
                 $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-                $mail->charSet = 'UTF-8';
+                // $mail->charSet = 'UTF-8';
                 //Recipients
-                $mail->setFrom('damvietvuong123@gmail.com', 'Vuong');
+                $mail->setFrom('dothong27112001@gmail.com', 'Thong');
                 $mail->addAddress($to, 'You');     //Add a recipient
                 // $mail->addAddress('ellen@example.com');               //Name is optional
                 // $mail->addReplyTo('info@example.com', 'Information');
